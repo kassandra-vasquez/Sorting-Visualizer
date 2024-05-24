ad73cbca9b59f5dad7e073530d8c6bd204082c02
kassandra <kyanelyv@yahoo.com>
2024-05-23 20:52:26 -0700

    bubble and merge sort showing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 from tkinter import ttk
 import random
 from colors import *
-
+from algorithms.bubblesort import bubble_sort
+from algorithms.mergesort import merge_sort
 
 # making a basic window
 window = Tk()
@@ -36,7 +37,7 @@ def drawData(data, colorArray):
         y1 = canvas_height
 
         canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
-        window.update_idletasks()
+    window.update_idletasks()
 
 # function will generate array with random values eveytime generate button hit
 def generate():
@@ -46,7 +47,7 @@ def generate():
     for i in range(0, 100):
         random_value = random.randint(1, 150)
         data.append(random_value)
-        drawData(data, [BLUE for x in range(len(data))])
+    drawData(data, [BLUE for x in range(len(data))])
 
 # function will set sorting speed
 def set_speed():
@@ -59,7 +60,14 @@ def set_speed():
 
 # function triggers selected algorithm and starts sorting
 def sort():
-    pass
+    global data
+    timeTick = set_speed()
+
+    if algo_menu.get() == "Bubble Sort":
+        bubble_sort(data, drawData, timeTick)
+    elif algo_menu.get() == "Merge Sort":
+        merge_sort(data, 0, len(data)-1, drawData, timeTick)
+
 
 ## UI HERE ##
 UI_frame = Frame(window, width=900, height=300, bg=WHITE)
@@ -81,6 +89,10 @@ speed_menu = ttk.Combobox(UI_frame, textvariable=speed_name, values=speed_list)
 speed_menu.grid(row=1, column=1, padx=5, pady=5)
 speed_menu.current(0)
 
+# canvas to draw array
+canvas = Canvas(window, width=800, height=400, bg=WHITE)
+canvas.grid(row=1, column=0, padx=10, pady=5)
+
 # the sort button
 B1 = Button(UI_frame, text="Sort", command=sort, bg=LIGHT_BEIGE)
 B1.grid(row=2, column=1, padx=5, pady=5)
@@ -88,10 +100,6 @@ B1.grid(row=2, column=1, padx=5, pady=5)
 # button that generates array
 B3 = Button(UI_frame, text="Generate Array", command=generate, bg=LIGHT_BEIGE)
 B3.grid(row=2, column=0, padx=5, pady=5)
-
-# canvas to draw array
-canvas = Canvas(window, width=800, height=400, bg=WHITE)
-canvas.grid(row=1, column=0, padx=10, pady=5)
 
 
 window.mainloop()
